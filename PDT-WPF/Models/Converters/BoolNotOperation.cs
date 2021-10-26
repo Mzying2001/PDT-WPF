050ac4c89ac2c457d8a62c0bdf683c42d2da4466
Mzying2001 <Mzying2001@qq.com>
2021-10-27 00:32:19 +0800

    使用Boolean2BooleanReConverter替换BoolNotOperation

--- PDT-WPF/Models/Converters/BoolNotOperation.cs
+++ PDT-WPF/Models/Converters/BoolNotOperation.cs
@@ -1,7 +1,9 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
 namespace PDT_WPF.Models.Converters
 {
+    [Obsolete("使用HandyControl的Boolean2BooleanReConverter替换")]
     public class BoolNotOperation : ValueConverterBase<bool, bool>
     {
         public override bool Convert(bool value, object parameter, CultureInfo culture)
