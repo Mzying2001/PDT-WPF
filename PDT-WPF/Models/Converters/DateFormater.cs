d23a9fe82d83e112bd2fb422363ecdf11dfe7f27
Mzying2001 <Mzying2001@qq.com>
2021-10-28 15:31:03 +0800

    调整日期显示

--- PDT-WPF/Models/Converters/DateFormater.cs
+++ PDT-WPF/Models/Converters/DateFormater.cs
@@ -7,38 +7,45 @@ namespace PDT_WPF.Models.Converters
     {
         public override string Convert(string value, object parameter, CultureInfo culture)
         {
-            DateTime d = DateTime.Parse(value);
+            try
+            {
+                DateTime d = DateTime.Parse(value);
 
-            DateTime now = DateTime.Now;
-            TimeSpan gap = now - d;
+                DateTime now = DateTime.Now;
+                TimeSpan gap = now - d;
 
-            if (gap.TotalSeconds < 60)
-            {
-                return $"{gap.Seconds}秒前";
+                if (gap.TotalSeconds < 60)
+                {
+                    return $"{gap.Seconds}秒前";
+                }
+                else if (gap.TotalMinutes < 60)
+                {
+                    return $"{gap.Minutes}分钟前";
+                }
+                else if (gap.TotalHours < 24)
+                {
+                    return $"{gap.Hours}小时前";
+                }
+                else if ((int)gap.TotalDays == 1)
+                {
+                    return "昨天";
+                }
+                else if ((int)gap.TotalDays == 2)
+                {
+                    return "前天";
+                }
+                else if (gap.TotalDays <= 7)
+                {
+                    return $"{(int)gap.TotalDays}天前";
+                }
+                else
+                {
+                    return d.ToString("yyyy/MM/dd");
+                }
             }
-            else if (gap.TotalMinutes < 60)
+            catch
             {
-                return $"{gap.Minutes}分钟前";
-            }
-            else if (gap.TotalHours < 24)
-            {
-                return $"{gap.Hours}小时前";
-            }
-            else if ((int)gap.TotalDays == 1)
-            {
-                return "昨天";
-            }
-            else if ((int)gap.TotalDays == 2)
-            {
-                return "前天";
-            }
-            else if (gap.TotalDays <= 7)
-            {
-                return $"{(int)gap.TotalDays}天前";
-            }
-            else
-            {
-                return d.ToString("yyyy/MM/dd");
+                return "???";
             }
         }
 
