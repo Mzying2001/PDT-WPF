1e369a2200b9c77e5f60e3abee28d50230149f8f
Mzying2001 <Mzying2001@qq.com>
2022-03-03 12:33:48 +0800

    添加InputStringDialog

---
+++ PDT-WPF/Models/Converters/StringNotEmpty.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace PDT_WPF.Models.Converters
+{
+    public class StringNotEmpty : ValueConverterBase<string, bool>
+    {
+        public override bool Convert(string value, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+
+        public override string ConvertBack(bool value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PDT-WPF/Views/Dialogs/InputStringDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+namespace PDT_WPF.Views.Dialogs
+{
+    /// <summary>
+    /// InputStringDialog.xaml 的交互逻辑
+    /// </summary>
+    public partial class InputStringDialog : Window
+    {
+        private bool _confirmed = false;
+        private Action<bool, string> _callback;
+
+
+        public string Input
+        {
+            get { return (string)GetValue(InputProperty); }
+            set { SetValue(InputProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Input.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty InputProperty =
+            DependencyProperty.Register("Input", typeof(string), typeof(InputStringDialog), new PropertyMetadata(null));
+
+
+        public InputStringDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _callback?.Invoke(_confirmed, Input);
+        }
+
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            _confirmed = true;
+            Close();
+        }
+
+        public static void ShowDialog(Action<bool, string> callback, string title = null)
+        {
+            new InputStringDialog()
+            {
+                _callback = callback,
+                Title = title ?? "请输入文本"
+            }.ShowDialog();
+        }
+    }
+}
