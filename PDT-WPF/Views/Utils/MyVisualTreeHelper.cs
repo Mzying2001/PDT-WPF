0cd47c25f2f63b942dcd114aa67e199aaf2da03d
Mzying2001 <Mzying2001@qq.com>
2022-03-03 17:41:08 +0800

    Update MyVisualTreeHelper.cs

--- PDT-WPF/Views/Utils/MyVisualTreeHelper.cs
+++ PDT-WPF/Views/Utils/MyVisualTreeHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
 using System.Windows.Media;
 
 namespace PDT_WPF.Views.Utils
@@ -10,6 +11,18 @@ namespace PDT_WPF.Views.Utils
             while (obj != null && !(obj is T))
                 obj = VisualTreeHelper.GetParent(obj);
             return obj as T;
+        }
+
+        public static T[] GetChildren<T>(DependencyObject obj) where T : DependencyObject
+        {
+            List<T> result = new List<T>();
+            for (int i = VisualTreeHelper.GetChildrenCount(obj) - 1; i >= 0; i--)
+            {
+                var item = VisualTreeHelper.GetChild(obj, i);
+                if (item is T child)
+                    result.Add(child);
+            }
+            return result.ToArray();
         }
     }
 }
